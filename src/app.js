ba133836c180f2ce4503aca016d33c6c60c5976a
Blackcend <mrcoeus@hotmail.com>
2017-06-27 10:11:17 +0700

    Send data via signal

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
 const openStream = require('./openStream');
-openStream();
+const Peer = require('simple-peer');
+const $ = require('jquery');
+//openStream();
+
+
+// tao token
+const p = new Peer({ initiator: location.hash === '#1', trickle: false });
+
+// signal offer
+p.on('signal', (token) => {
+    $("#txtMySignal").val(JSON.stringify(token));
+});
+
+// signal answer
+p.on('connect',()=>{
+    setInterval(()=>p.send(Math.random),2000);
+});
+
+// Khi co data
+p.on('data',data => console.log('Nhan du lieu: ' + data));
+
+$("#btnConnect").click(()=>{
+    const friendSignal = JSON.parse($("#txtFriendSignal").val());
+    p.signal(friendSignal);
+});
+
 console.log('Hello World');
